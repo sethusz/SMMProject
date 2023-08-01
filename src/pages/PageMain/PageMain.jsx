66a76843589c91c683b3b4ae9ae3254d389d649a
d9m0n4 <c2h8e9d4@gmail.com>
2023-08-01 19:18:58 +0300

    qwe

--- src/pages/PageMain/PageMain.jsx
+++ src/pages/PageMain/PageMain.jsx
@@ -22,14 +22,13 @@ const PageMain = () => {
       <div className="image__container image">
         <div className="image__content">
           <img src={img_main} alt="" className="image__main" />
-          <div className="image__text">КУРС ПО СММ ОТ ОРАЗБЕКОВОЙ АСЕЛЬ.</div>
+          <div className="image__text">КУРС ПО ВИЗУАЛУ&SMM АСЕЛЬ ОРАЗБЕКОВОЙ.</div>
           <div className="image__arrow" onClick={scrollToMainContent}></div>
         </div>
       </div>
 
       <div className="main__content main" ref={mainContentRef}>
         <div className="main__title">VISUAL&STORIES</div>
-
         <hr />
 
         <SmmModules />
@@ -37,18 +36,10 @@ const PageMain = () => {
 
       <div className="footer">
         <div className="footer__section">
-          <div className="footer__about-title">О нас</div>
-          <div className="footer__about">
-            {' '}
-            Здесь можно написать о вашей компании или о ваших услугах.
-          </div>
-        </div>
-        <div className="footer__section">
           <div className="footer__contact-title">Контакты</div>
           <ul className="footer__contacts">
-            <li className="footer__contact">Email: example@example.com</li>
-            <li className="footer__contact">Телефон: 123-456-789</li>
-            <li className="footer__contact">Адрес: Город, Улица, Дом</li>
+            <li className="footer__contact">instagram: example@example.com</li>
+            <li className="footer__contact">telegram: 123-456-789</li>
           </ul>
         </div>
       </div>
